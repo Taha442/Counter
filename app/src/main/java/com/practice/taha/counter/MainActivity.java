cd16030a91dd6e3ee7c3064ee33b70b7caf0208c
Taha442 <tmohuddin@hotmail.com>
2017-01-29 06:57:06 -0500

    Test 1

---
+++ app/src/main/java/com/practice/taha/counter/MainActivity.java
@@ -0,0 +1,117 @@
+package com.practice.taha.counter;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    boolean pressed;
+    TextView value;
+    CharSequence original;
+    int num;
+    Button add;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        //add = (Button)findViewById(R.id.add1);
+        num = Integer.parseInt(original.toString());
+        original = value.getText();
+        value = (TextView) findViewById(R.id.Counter);
+        pressed = false;
+
+        add.setOnTouchListener(bob);
+
+        //while(pressed){
+          //  num++;
+        //    value.setText(String.valueOf(num));
+      //  }
+
+    }
+
+
+    public void increment(View view){
+        TextView value = (TextView) findViewById(R.id.Counter);
+        CharSequence original = value.getText();
+        int num = Integer.parseInt(original.toString());
+        num++;
+
+        value.setText(String.valueOf(num));
+    }
+    public void decrement(View view){
+        TextView value = (TextView) findViewById(R.id.Counter);
+        CharSequence original = value.getText();
+        int num = Integer.parseInt(original.toString());
+        num--;
+        value.setText(String.valueOf(num));
+    }
+
+
+        //TextView value = (TextView) findViewById(R.id.Counter);
+        //
+View.OnTouchListener bob = new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                       switch(event.getAction()){
+                       case MotionEvent.ACTION_DOWN:
+                        pressed = true;
+                        num++;
+
+                        value.setText(String.valueOf(num));
+
+                    case MotionEvent.ACTION_UP:
+                        pressed = false;
+
+                }
+
+                return true;
+
+            }
+        };
+
+
+       // num--;
+       // value.setText(String.valueOf(num));
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
